7eda698b4c3fd79cd6bec1369c06bdc73f6034ab
Sabrina Spruck <113223660+SabrinaS0909@users.noreply.github.com>
2025-02-10 22:53:04 -0500

    Coming up with how to make it mine

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,1 +1,15 @@
 """This is a small project of my own take on a game of Rock, Paper, Scissors"""
+
+import random
+
+"""So, this is where I want to decide how to implement my own twist on a very classic game.
+I can choose the same gameplay just with my own name for everything, but I feel that would be boring.
+Can I implement at least one more twist on things. To the mechanics, maybe.
+We can add more objects, for example if we have rock, paper scissors, then we can add in more objects that interact with the current objects in meaningful ways. Let's aim for two of these.
+Then, maybe, we could add in some sort of wildcard. A new mechanic. Maybe a tie breaker, even. Maybe if two choose the same object initially, it decends into a secondary game. And maybe even from there a thrid or fourth if the luck were so.
+So we've gotta choose something large, then scope in at least four times.
+I'm thinking three creatures, then three elements, then going into composition of their matter (solid, gas and liquid), then from there... hmm.
+Then, lastly, neutron, proton and electrons feels like the most obvious place to go from there. 
+Another factor could be age. That could be a more simple dynamic, that plays better with what common people are down to deal with."""
+
+player_action = input("Enter a choice")
