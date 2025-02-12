b657afae9dd6fd8d8ecd8a95d4a38d9a6620a919
Sabrina Spruck <113223660+SabrinaS0909@users.noreply.github.com>
2025-02-11 23:33:19 -0500

    Completed basic code for the game
    And started adding in the parts that make it more unique and different from the original game.

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -12,4 +12,64 @@ I'm thinking three creatures, then three elements, then going into composition o
 Then, lastly, neutron, proton and electrons feels like the most obvious place to go from there. 
 Another factor could be age. That could be a more simple dynamic, that plays better with what common people are down to deal with."""
 
-player_action = input("Enter a choice")
+"""Now to keep mistakes from happening, I need to change the objects to my own and maintain, rather than putting in rock, paper and scissors and potentially missing replacing one.
+I think the animal that is the "weakest" but can also take out the strongest should be a bee.
+So bee = paper, then raven eats the bee and is therefore scissors, then maybe a cat - then because I want to extend it out a little bit, let's add two more animals and see if it overcomplicated the coding process.
+Maybe wolf, then human. But the human can get stung by the bee and therefore loses to the bee."""
+
+player_action = input("Enter your animal of choice: Bee, Corvid, Cat, Wolf, Human\n")
+#when we put css in, I would like to add pictures of the animals of choice that the player can choose from. Maybe even randomize the photo shown each playthrough.
+possible_actions = ["bee", "corvid", "cat", "wolf", "human"]
+computer_action = random.choice(possible_actions)
+
+print(f"\nIt's {player_action} vs {computer_action}.\n")
+
+if player_action == computer_action:
+    #This one is where the differentiating mechanics come in. And the game goes deeper.
+elif player_action == "bee":
+    if computer_action == "corvid":
+        print("Corvid eats the bee! You lose!")
+    elif computer_action == "cat":
+        print("Cat paws the bee to death, you lose!")
+    elif computer_action == "wolf":
+        print("Wolf gets stung in the muzz and looks silly the rest of the day. You win!")
+    else:
+        print("Human is allergic to the bee and is hospitalized. You win!")
+elif player_action == "corvid":
+    if computer_action == "bee":
+        print("Bee is eaten by the corvid! You win!")
+    elif computer_action == "cat":
+        print("Cat stalks and kills its prey, you lose!")
+    elif computer_action == "wolf":
+        print("Wolf befriends the corvid and they hunt prey together, you win!")
+    else:
+        print("Human destroys the corvid's natural habitat and it's forced to live in the city. You lose!")
+elif player_action == "cat":
+    if computer_action == "bee":
+        print("Bee gets pawed to death by cat! You win!")
+    elif computer_action == "corvid":
+        print("Corvid is stalked and killed by the highly invasive cat. You win!")
+    elif computer_action == "wolf":
+        print("Wolf steals the cat wandering it's territory for dinner. You lose!")
+    else:
+        print("Human domesticates cat and makes it wear silly hats and posts pictures of the embarassing situation all over the internet. You lose!")
+elif player_action == "wolf":
+    if computer_action == "corvid":
+        print("Corvid befriends the wolf and alerts it to prey for both to enjoy. You win!")
+    elif computer_action == "cat":
+        print("Cat navigates the forest and is stolen by wolf for dinner. You win!")
+    elif computer_action == "bee":
+        print("Bee stings wolf in the muzz and it looks silly the rest of the day. You lose!")
+    else:
+        print("Human tests wolf boundaries with a meat-suit and gets mauled. You win!")
+#what if we made the human a wild card where it could be randomized that you could win or lose
+elif player_action == "human":
+    if computer_action == "corvid":
+        print("Corvid drops from the sky due to human neonicotinoids. Well, at least the bugs are gone from its apples. You win!")
+    elif computer_action == "cat":
+        print("Cat paws the bee to death, you lose!")
+    elif computer_action == "wolf":
+        print("Wolf gets stung in the muzz and looks silly the rest of the day. You win!")
+    else:
+        print("Human is allergic to the bee and is hospitalized. You win!")
+
