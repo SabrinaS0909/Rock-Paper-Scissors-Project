76173ac54908b7c1cdf5dd4cf7727415b6ea13de
Sabrina Spruck <113223660+SabrinaS0909@users.noreply.github.com>
2025-03-08 22:47:55 -0500

    buttons are working

--- rock_paper_scissors_2.py
+++ rock_paper_scissors_2.py
@@ -1,43 +1,11 @@
-# import packages
-import pygame
-from pygame.locals import *
-from AnimalButton import *
-import sys
+from flask import Flask, request, jsonify
 
-# define constants
-background_color = ("#90EE90")
-window_width = "100%"
-window_height = "100%"
-frames_per_second = 30
+app = Flask(__name__)
 
-# initialize the world
-pygame.init()
-window = pygame.display.set_mode((window_width, window_height))
-clock = pygame.time.Clock()
+@app.route('/button_click', methods = ['POST'])
+def button_click():
+    print("Button pressed on server")
+    return jsonify ({"message": "Button click received"})
 
-# load assets: images(s), sounds, etc.
-
-# initialize variables
-
-# create an instance of SimpleButton - we will rename to AnimalButton
-objectButton = AnimalButton(window, (75, 80),
-                            'images/buttonUp.png',
-                            'images/buttonDown.png')
-
-# loop forever
-while True:
-
-    # check for and handle events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        
-        # pass the event to the button
-        if objectButton.handleEvent(event):
-            print('User has clicked the button.')
-
-    # do any "per frame" actions
-
-    # clear the window
-    window.fill(background_color)
+if __name__ == '__main__':
+    app.run(debug=True)
